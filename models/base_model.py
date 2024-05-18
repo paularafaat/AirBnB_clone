eebd4387f8f31136d56c1e66524dce83d72b7e80
paula raafat <paularaafat27@.com>
2024-05-18 22:05:24 +0300

    AIR BnB

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,13 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+    
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+        Format: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
