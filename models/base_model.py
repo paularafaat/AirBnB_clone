9b68524dd0b4d4f105293e7213acdd7f8f943831
paula raafat <paularaafat27@.com>
2024-05-19 00:50:43 +0300

    AIR BnB

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,29 @@ from datetime import datetime
 
 class BaseModel:
     """BseModel Class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
         The id attribute is assigned a unique UUID string.
         The created_at and updated_at attributes are
         assigned the current datetime.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """
