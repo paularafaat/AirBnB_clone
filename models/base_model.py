7f7be1f9640b6cfef31d08ebfbe165748995e5ce
paula raafat <paularaafat27@.com>
2024-05-19 20:59:02 +0300

    AIR BnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """base_modle module"""
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class BaseModel:
@@ -22,12 +22,12 @@ class BaseModel:
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
             if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
+                self.created_at = datetime.now(timezone.utc)
             if 'updated_at' not in kwargs:
                 self.updated_at = self.created_at
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now(timezone.utc)
 
     def save(self):
         """
@@ -36,7 +36,7 @@ class BaseModel:
         This method should be called every
         time an object is changed.
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now(timezone.utc)
 
     def to_dict(self):
         """
@@ -58,5 +58,5 @@ class BaseModel:
         Returns a string representation of the instance.
         Format: [<class name>] (<self.id>) <self.__dict__>
         """
-        class_namae = self.__class__.__name__
-        return f"[{class_namae}] ({self.id}) {self.__dict__}"
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
