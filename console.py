96e859a64da78066e1f6f3fb31ac16db403de00f
paula raafat <paularaafat27@.com>
2024-05-20 02:15:37 +0300

    AIR BnB

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""console module"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+    prompt = "(hbnb)"
+    def do_quit(self, arg):
+        """quiting the console"""
+        return True
+
+    def help_quit(self):
+        """Help information for the quit command"""
+        print("Quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """Handle EOF to exit the program"""
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
